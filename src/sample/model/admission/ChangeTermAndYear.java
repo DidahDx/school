b0b162de08ad508ad4682580b93bc8ac49662a7c
Daniel Didah <didahdaniel@gmail.com>
2019-04-26 16:21:43 +0300

    Added method to set the Term

--- src/sample/model/admission/ChangeTermAndYear.java
+++ src/sample/model/admission/ChangeTermAndYear.java
@@ -1,15 +1,19 @@
 package sample.model.admission;
 
+import sample.dataAccessObject.admission.StudentDao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Calendar;
 
 public class ChangeTermAndYear {
 
-    LocalDate today=LocalDate.now();
+StudentDao stDao=new StudentDao();
 
+    //this method is used to check the current term
     public int CheckTerm(){
-     int term = 0;
+        int term = 0;
        int month=Calendar.getInstance().get(Calendar.MONTH) +1;
 
       if ((month>=1)&& (month<=4) )
@@ -27,7 +31,57 @@ public class ChangeTermAndYear {
     }
 
 
+    //this method is used to set the term
     public void SetTerm(){
+        int currentTerm=CheckTerm();
+        int DBterm = 0;
+
+        try {
+         ResultSet rs= stDao.getTerm();
+         while (rs.next()){
+             DBterm=rs.getInt("current_term");
+         }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (currentTerm>DBterm){
+            if (!(currentTerm==DBterm)){
+                 //updates term to currentTerm
+                try {
+                    stDao.setTerm(currentTerm);
+                }
+                catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        else if((currentTerm==1) && (DBterm==3)){
+                //updates term to currentTerm
+            try {
+                stDao.setTerm(currentTerm);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    //this method is used to check the current year
+    public int CheckYear(){
+        int year;
+
+        LocalDate today=LocalDate.now();
+        year=today.getYear();
+
+        return  year;
+    }
+
+/** TODO: COMPLETE WRITING CHANGE STUDENT FORM*/
+    //this method is used to change the students form
+    public void ChangeStudentForm(){
 
     }
 
